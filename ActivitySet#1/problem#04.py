c40318e72c0a59d3f24e45bf5764565044f67ecc
Arjitjain1 <arjit178@gmail.com>
2022-05-15 16:16:41 +0530

    pay program using if statement

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,11 @@
-# Conditional Execution
-
-hrs = input("Enter hours? ")
+hrs = input("Enter Hours:")
+rate = input("Enter Rate:")
+hrs =float(hrs)
+rate =float(rate)
+if hrs>40:
+    regular = rate * hrs
+    overtime = (hrs-40.0)*(rate*0.5) 
+    pay = regular + overtime 
+else:
+    pay = hrs * rate
+print("Pay:",pay) 
